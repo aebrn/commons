7a62dad056fbbecad7dbcdd2fa9274dc59fece0e
highimhell <brandonallan6@gmail.com>
2021-01-11 12:59:54 +0300

    Update Timer.java

--- main/java/io/aebrn/commons/timing/Timer.java
+++ main/java/io/aebrn/commons/timing/Timer.java
@@ -1,25 +1,42 @@
 package io.aebrn.commons.timing;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author aebrn
  * 12/13/2020
  */
 public final class Timer {
-    private long previousMS;
+    private final TimeUnit timeUnit;
 
-    protected Timer() {
+    private long previous;
+
+    protected Timer(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
         reset();
     }
 
-    public boolean hasElapsed(long milliseconds) {
-        return getCurrentMS() - this.previousMS >= milliseconds;
+    public boolean hasElapsed(long nanoseconds) {
+        return getCurrentTime() - this.previous >= convertToUnit(nanoseconds);
+    }
+
+    private long convertToUnit(long nanoseconds) {//TODO test this thoroughly
+        return switch (timeUnit) {
+            case NANOSECONDS -> TimeUnit.NANOSECONDS.convert(nanoseconds, TimeUnit.NANOSECONDS);
+            case MICROSECONDS -> TimeUnit.MICROSECONDS.convert(nanoseconds, TimeUnit.NANOSECONDS);
+            case MILLISECONDS -> TimeUnit.MILLISECONDS.convert(nanoseconds, TimeUnit.NANOSECONDS);
+            case SECONDS -> TimeUnit.SECONDS.convert(nanoseconds, TimeUnit.NANOSECONDS);
+            case MINUTES -> TimeUnit.MINUTES.convert(nanoseconds, TimeUnit.NANOSECONDS);
+            case HOURS -> TimeUnit.HOURS.convert(nanoseconds, TimeUnit.NANOSECONDS);
+            case DAYS -> TimeUnit.DAYS.convert(nanoseconds, TimeUnit.NANOSECONDS);
+        };
     }
 
     public void reset() {
-        this.previousMS = getCurrentMS();
+        this.previous = getCurrentTime();
     }
 
-    private long getCurrentMS() {
+    private long getCurrentTime() {
         return System.nanoTime() / 1000000L;
     }
 }
